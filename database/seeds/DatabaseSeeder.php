36a0d4c7c6a064ae826f0a0eac1b4e580758cac9
Rohmat Hidayattullah <rohmathidayattullah97@gmail.com>
2023-04-12 12:52:17 +0700

    add user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(TransactionsTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
