f1fb95b25086fc117b231dd3d10025a9f5eeeb12
rohmatHT8 <rohmathidayattullah97@gmail.com>
2025-03-05 11:54:28 +0700

    test

--- app/Repositories/ItemTypeRepositoryEloquent.php
+++ app/Repositories/ItemTypeRepositoryEloquent.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Criteria\RequestCriteria;
-use App\Repositories\itemTypeRepository;
+use App\Repositories\ItemTypeRepository;
 use App\Entities\ItemType;
 use App\Validators\ItemTypeValidator;
 
