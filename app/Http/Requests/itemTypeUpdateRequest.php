cf57886ca6c77c4f5eb6bade4b7564a45080bfbd
rohmatHT8 <rohmathidayattullah97@gmail.com>
2025-03-05 13:53:03 +0700

    done

--- app/Http/Requests/itemTypeUpdateRequest.php
+++ app/Http/Requests/itemTypeUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class itemTypeUpdateRequest extends FormRequest
+class ItemTypeUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
